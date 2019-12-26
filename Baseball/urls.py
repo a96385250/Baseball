0abe0830fcc92453881faf0273ea9acaebadf86a
aien0313 <aien0313@iii.org.tw>
2019-12-26 21:32:36 +0800

    design basic html and css

--- Baseball/urls.py
+++ Baseball/urls.py
@@ -18,5 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('baseball_home.urls'))
+    path('',include('baseball_home.urls')),
+    path('member/',include('member.urls'))
 ]
